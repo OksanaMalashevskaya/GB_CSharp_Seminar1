82f4d7e63f1b4c7ae4e93f59143346c468d09b52
oksana <malashevskaya.oksana@gmail.com>
2023-10-07 20:33:52 +0300

    Задача 4 решена. Автотест пройден.

---
+++ Task_004/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+        int start = 1;
+        while (start <= number)
+        {
+            if (start % 2 ==0){
+                Console.Write(start.ToString()+"\t");
+            }
+            start+=1;   
+        }
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 6;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}
