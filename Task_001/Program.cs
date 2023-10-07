a9a30002be5d5189a18c8d22fb6611a5f80ce397
oksana <malashevskaya.oksana@gmail.com>
2023-10-07 19:27:25 +0300

    Задача 1 решена. Автотест пройден.

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        if (firstNumber == secondNumber){
+            Console.WriteLine("Введенные числа равны `" + firstNumber.ToString()+"`");
+        } else {
+            if (firstNumber < secondNumber){
+                Console.WriteLine("Первое число `" + firstNumber.ToString() + "` меньше чем второе число `"+ secondNumber.ToString()+"`");
+            } else {
+                Console.WriteLine("Первое число `" + firstNumber.ToString() + "` больше чем второе число `"+ secondNumber.ToString()+"`");   
+            }
+        }
+
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 7;
+            secondNumber = 3;
+        }
+
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
+}
